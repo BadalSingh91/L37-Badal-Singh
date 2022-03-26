32307a783f2a597f621f907f9f72e82fad353ca1
BADAL91 <102398757+BadalSingh91@users.noreply.github.com>
2022-03-26 03:52:51 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@TragicGlaringParallelport/main.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+  float unti,total,charge;
+  char name[20];
+  printf("Enter the units:");
+  scanf("%f,&unit");
+  charge=0;
+  if(unit>+1&&unit<=200)
+{
+  charge=0.8*unit;
+}
+else if(unit>=201&&unit<=300)
+{
+  charge=0.8*(200)+0.9*(unit-200);
+}
+else if(unit>300)
+{
+  charge=0.8*(200)+0.9*(100)+1*(unit-300);
+}
+total=charge+100;
+if(total>400)
+{
+  total=total+(0.15*total);
+}
+printf("\nName:%s\n",name);
+printf("No. of units:%2f\n");
+printf("Total Bill Amount:RS.%sf\n",total);
+printf("")
